8fcebd69f3b29966bedd8669d3a2fce0dccf5c3a
Gögi <goekce.kavel@gmail.com>
2024-10-11 20:38:23 +0200

    trying streamlit

---
+++ test.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.write('Hi')
+st.header('Simple demo')
+st.image("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.mMWuzEPKzws6n-oi_7pAyQHaEo%26pid%3DApi&f=1&ipt=d16283a6afa2a079f31feead96b17afb773e3b58178aedc874f4b86a0926545f&ipo=images")
